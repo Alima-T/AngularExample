01ae09a47ae055b007ae8346e3f50b44166c7ea5
Alima-T <okhapka.a@gmail.com>
2023-05-23 20:18:49 +0300

    #8 refactoring - display of tasks by category using reactive RxJS

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import {Category} from "../model/Category";
 import {TestData} from "../data/TestData";
 import { Task } from '../model/Task';
+import {Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
+
+  tasksSubject = new Subject<Task[]>();
+
 
   constructor() { }
 
@@ -14,14 +18,24 @@ export class DataHandlerService {
     return TestData.categories;
   }
 
-  getTask(): Task[]{
-    return TestData.tasks;
+
+  // getTask(): Task[]{
+  //   return TestData.tasks;
+  // }
+
+  // instead of getTask
+    fillTasks(){
+    this.tasksSubject.next(TestData.tasks);
   }
 
-  getTaskByCategory(category: Category): Task[]{
-    const tasks = TestData.tasks.filter(task => task.category === category);
-    console.log(tasks);
-    return tasks;
+  // getTaskByCategory(category: Category): Task[]{
+  //   const tasks = TestData.tasks.filter(task => task.category === category);
+  //  // console.log(tasks);
+  //   return tasks;
+  // }
+// instead of getTaskByCategory
+  fillTasksByCategory(category: Category){
+    this.tasksSubject.next(TestData.tasks.filter(task=>task.category===category))
   }
 
 }
