09569856339268c0bbdbbe4b1cd7b0e20a0e3c98
Alima-T <okhapka.a@gmail.com>
2023-05-26 18:56:22 +0300

    #14 set styles for active tasks and added pointer

--- src/app/view/categories/category.component.ts
+++ src/app/view/categories/category.component.ts
@@ -11,6 +11,8 @@ export class CategoryComponent implements OnInit{
 //latest version of TypeScript includes a strict class checking where all the properties should be initialized in the constructor. A workaround is to add the ! as a postfix to the variable name.
   categories: Category[]=[];
 
+  selectedCategory: Category | undefined;
+
 // in the constructor, we add the variable name: Type to the parameters, subsequently, when creating an instance of category.component, a reference and access to an instance of the DataHandlerService class will be embedded in it
   constructor(private dataHandler: DataHandlerService) {
   }
@@ -24,6 +26,7 @@ export class CategoryComponent implements OnInit{
   }
 
   displayTasksByCategory(category: Category) {
+    this.selectedCategory = category;
     this.dataHandler.fillTasksByCategory(category);
   }
 }
